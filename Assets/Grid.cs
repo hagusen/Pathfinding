80b1eb4cbafddb794ea3b2d54aa6ce8e098df9a2
hagusen <john@bit-ab.com>
2020-09-20 22:45:11 +0200

    Worked on grid

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Grid : MonoBehaviour
 {
 
-    public Node[,] nodes;
+    public Node[,] grid;
 
     public Vector2Int gridWorldsize;
     public float resolution; // 1/x
@@ -21,13 +21,38 @@ public class Grid : MonoBehaviour
 
         gridsizeX = Mathf.RoundToInt(gridWorldsize.x / nodeDiameter);
         gridsizeY = Mathf.RoundToInt(gridWorldsize.y / nodeDiameter);
+
+        CreateGrid();
     }
 
+
+    void CreateGrid() {
+        grid = new Node[gridsizeX, gridsizeY];
+        //Vector3 anchorPoint = new Vector3(gridWorldsize.x / 2f, 0, gridWorldsize.y / 2f);
+
+        for (int i = 0; i < gridsizeX; i++) {
+            for (int k = 0; k < gridsizeY; k++) {
+
+                Vector3 pos = Vector3.right * (i + nodeRadius) + Vector3.forward * (k + nodeRadius); // in the middle of each node
+                grid[i, k] = new Node(pos, true);
+            }
+        }
+
+
+    }
 
 
 
     void OnDrawGizmos() { // use only x -> x%2 = 0 or x& binary
         Gizmos.DrawWireCube(new Vector3(gridWorldsize.x/2f, 0, gridWorldsize.y/2f), new Vector3(gridWorldsize.x, 2, gridWorldsize.y));
+
+
+        if (grid != null) {
+            foreach (var node in grid) {
+                Gizmos.DrawCube(node.worldPositon, Vector3.one * 0.2f);
+            }
+        }
+
     }
 
 
