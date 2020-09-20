1a39a867c2240527fec728ec52f46d3159bce9c8
hagusen <john@bit-ab.com>
2020-09-20 20:28:32 +0200

    started

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -2,9 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid
+public class Grid : MonoBehaviour
 {
 
-    Node[,] nodes;
+    public Node[,] nodes;
+
+    public Vector2Int gridWorldsize;
+    public float resolution; // 1/x
+
+    public float nodeRadius;
+    float nodeDiameter;
+
+    int gridsizeX, gridsizeY;
+
+    private void Start() {
+
+        nodeDiameter = nodeRadius * 2;
+
+        gridsizeX = Mathf.RoundToInt(gridWorldsize.x / nodeDiameter);
+        gridsizeY = Mathf.RoundToInt(gridWorldsize.y / nodeDiameter);
+    }
+
+
+
+
+    void OnDrawGizmos() { // use only x -> x%2 = 0 or x& binary
+        Gizmos.DrawWireCube(new Vector3(gridWorldsize.x/2f, 0, gridWorldsize.y/2f), new Vector3(gridWorldsize.x, 2, gridWorldsize.y));
+    }
+
 
 }
--- Assets/Node.cs
+++ Assets/Node.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Node
 {
+    public bool walkable;
+    public Vector3 worldPositon;
 
-
+    public Node(Vector3 _worldPosition, bool isWalkable) {
+        worldPositon = _worldPosition;
+        walkable = isWalkable;
+    }
 
 }
